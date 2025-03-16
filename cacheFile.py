7070acda0b87c95ffca20b55c1d288afde550ef9
Black_Dragon <67070059+mostafa-12@users.noreply.github.com>
2025-03-16 03:50:28 +0200

    'cache' part is done

--- cacheFile.py
+++ cacheFile.py
@@ -1,2 +1,56 @@
+import os
+import json
+import time
 class Cache:
-    pass
+    CACHE_FILE = "cache.json"
+    EXPIRATION_TIME = 3600
+    
+    @classmethod
+    def loadCache(cls):
+
+        if os.path.exists(cls.CACHE_FILE):
+        
+            with open(cls.CACHE_FILE) as file:
+        
+                return json.load(file)
+        return {}
+    
+    @classmethod
+    def save(cls, data):
+        
+        with open(cls.CACHE_FILE,"w") as file:
+            json.dump(data, file, indent=2, sort_keys= True)
+
+    @classmethod
+    def check(cls,paramter):
+        cache = cls.loadCache()
+
+        if paramter in cache.keys():
+
+            timeStamp = time.time() - cache[paramter]["timestamp"]
+            
+            if timeStamp >= cls.EXPIRATION_TIME:
+
+                del cache[paramter]
+            
+            else :
+                return cache[paramter]["data"]
+            
+        return {}
+
+    @classmethod
+    def update(cls, key, data):
+        
+        loaded_cache = cls.loadCache()
+
+        loaded_cache[key] = {
+
+            "data" : data,
+            "timestamp" : time.time()
+        }
+        cls.save(loaded_cache)
+        
+    @classmethod
+    def clearCache(cls):
+        if os.path.exists(cls.CACHE_FILE):
+            os.remove(cls.CACHE_FILE)
